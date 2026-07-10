07bde4fbe5562a58789a04a1be362ab573786511
Fidel <trabajosfidel4@gmail.com>
2026-07-09 22:28:16 -0300

    PSX: diagnostico de vertex-colors/materiales en el listador del FBX

--- Assets/editor/MapGenerator/PsxForestHelper.cs
+++ Assets/editor/MapGenerator/PsxForestHelper.cs
@@ -28,11 +28,27 @@ namespace FolkloreArchives.MapGen
             var sb = new StringBuilder("Contenido del FBX PSX (StarkCrafts):\n");
             foreach (var mf in fbx.GetComponentsInChildren<MeshFilter>(true))
             {
-                int tris = mf.sharedMesh != null ? mf.sharedMesh.triangles.Length / 3 : 0;
-                var b = mf.sharedMesh != null ? mf.sharedMesh.bounds.size : Vector3.zero;
-                sb.AppendLine($"  - {mf.gameObject.name}  (tris≈{tris}, alto≈{b.y:0.0})");
+                var mesh = mf.sharedMesh;
+                if (mesh == null) continue;
+                int tris = mesh.triangles.Length / 3;
+                int nCol = mesh.colors.Length;             // ← ¿tiene vertex-colors?
+                var mr = mf.GetComponent<MeshRenderer>();
+                string mats = "";
+                if (mr != null) foreach (var m in mr.sharedMaterials)
+                    mats += (m != null ? m.name + "[" + (m.shader != null ? m.shader.name : "?") + "]" : "null") + " ";
+                sb.AppendLine($"  - {mf.gameObject.name}  tris≈{tris}  vertexColors={nCol}  subMesh={mesh.subMeshCount}  mats: {mats}");
             }
+            // color del primer vértice del primer árbol (si hay)
+            var t1 = FindT("PSX_Tree1", fbx);
+            if (t1 != null && t1.colors.Length > 0) sb.AppendLine("PSX_Tree1 color[0] = " + t1.colors[0]);
             Debug.Log(sb.ToString());
+        }
+
+        static Mesh FindT(string name, GameObject fbx)
+        {
+            foreach (var mf in fbx.GetComponentsInChildren<MeshFilter>(true))
+                if (mf.gameObject.name == name) return mf.sharedMesh;
+            return null;
         }
     }
 }
